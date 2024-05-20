4fa1ddc65c3d47ece1861f5abf80274d20ff4714
parlyresk <parlyresk325@gmail.com>
2024-05-20 20:32:14 +0900

    bookshop

--- jdbc-practices/src/main/java/bookshop/dao/AuthorDao.java
+++ jdbc-practices/src/main/java/bookshop/dao/AuthorDao.java
@@ -11,54 +11,94 @@ import java.util.List;
 import bookshop.vo.AuthorVo;
 
 public class AuthorDao {
-
 	private Connection getConnection() throws SQLException {
-		Connection con = null;
-
+		Connection conn = null;
+		
 		try {
-			// 1. jdbc driver 로딩
 			Class.forName("org.mariadb.jdbc.Driver");
-
-			// 2. 연결하기
 			String url = "jdbc:mariadb://192.168.0.212:3306/webdb?charset=utf8";
-			con = DriverManager.getConnection(url, "webdb", "webdb");
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
 		} catch (ClassNotFoundException e) {
-
-			System.out.println("드라이버 로딩 실패: " + e);
+			System.out.println("드라이버 로딩 실패:" + e);
 		}
-
-		return con;
+		
+		return conn;
 	}
-
+	
 	public List<AuthorVo> findAll() {
 		List<AuthorVo> result = new ArrayList<>();
-
-		try (Connection con = getConnection();
-				PreparedStatement pstmt = con.prepareStatement("select no,name from author");
-				ResultSet rs = pstmt.executeQuery();) {
-
-			// 5. SQL 실행
-
-			// 6. 결과처리
-			while (rs.next()) {
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("select no, name from author");
+			ResultSet rs = pstmt.executeQuery();
+		) {
+			while(rs.next()) {
 				Long no = rs.getLong(1);
 				String name = rs.getString(2);
 				
-				AuthorVo vo=new AuthorVo();
+				AuthorVo vo = new AuthorVo();
 				vo.setNo(no);
 				vo.setName(name);
-
+				
 				result.add(vo);
 			}
-
-			
-
 		} catch (SQLException e) {
-
-			System.out.println("error: " + e);
-
+			System.out.println("error:" + e);
 		}
+		
 		return result;
 	}
 
+	public int insert(AuthorVo vo) {
+		int result = 0;
+
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt1 = conn.prepareStatement("insert into author(name) values(?)");
+			PreparedStatement pstmt2 = conn.prepareStatement("select last_insert_id() from dual");
+		) {
+			pstmt1.setString(1, vo.getName());
+			result = pstmt1.executeUpdate();
+			
+			ResultSet rs = pstmt2.executeQuery();
+			vo.setNo(rs.next() ?  rs.getLong(1) : null);
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		
+		return result;
+	}
+
+	public int deleteByNo(Long no) {
+		int result = 0;
+
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("delete from author where no = ?");
+		) {
+			pstmt.setLong(1, no);
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		
+		return result;		
+	}
+
+	public int deleteAll() {
+		int result = 0;
+		
+		try (
+			Connection conn = getConnection();
+			PreparedStatement pstmt = conn.prepareStatement("delete from author");
+		) {
+			result = pstmt.executeUpdate();
+		} catch (SQLException ex) {
+			System.out.println("error:" + ex);
+		}
+		
+		return result;
+	}
 }
--- jdbc-practices/src/test/java/bookshop/dao/BookDaoTest.java
+++ jdbc-practices/src/test/java/bookshop/dao/BookDaoTest.java
@@ -11,35 +11,51 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import bookshop.vo.AuthorVo;
+import bookshop.vo.BookVo;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class AuthorDaoTest {
+public class BookDaoTest {
 	private static int count = 0;
-	private static AuthorDao authorDao = new AuthorDao();
 	private static AuthorVo mockAuthorVo = new AuthorVo();
+	private static BookVo mockBookVo=new BookVo();
+	
+	private static AuthorDao authorDao = new AuthorDao();
+	private static BookDao bookDao = new BookDao();
 	
 	@BeforeAll
 	public static void setUp() {
-		count = authorDao.findAll().size();
-	}
-
-	@Test
-	@Order(1)
-	public void testInsert() {
 		mockAuthorVo.setName("칼세이건");
 		authorDao.insert(mockAuthorVo);
-
-		assertNotNull(mockAuthorVo.getNo());
+		
+		count = bookDao.findAll().size();
 	}
 	
 	@Test
-	@Order(2)	
+	@Order(1)
+	public void testInsert() {
+		
+		mockBookVo.setTitle("코스모스");
+		mockBookVo.setAuthorNo(mockAuthorVo.getNo());
+		bookDao.insert(mockBookVo);
+		
+		assertNotNull(mockBookVo.getNo());
+	}
+	
+	@Test
+	@Order(2)
 	public void testFindAll() {
-		assertEquals(count + 1, authorDao.findAll().size());
+		assertEquals(count + 1, bookDao.findAll().size());
+	}
+	
+	@Test
+	@Order(3)
+	public void testUpdate() {
+		assertEquals(1,bookDao.update(mockBookVo.getNo(),"대여중"));
 	}
 	
 	@AfterAll
 	public static void cleanUp() {
+		bookDao.deleteByNo(mockBookVo.getNo());
 		authorDao.deleteByNo(mockAuthorVo.getNo());
 	}
 }
